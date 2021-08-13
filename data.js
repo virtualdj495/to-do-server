399c68187ac3bb721a19491a7de3ca0f5789425b
virtualdj495 <catalin495@gmail.com>
2021-08-13 14:27:37 +0300

    token protection

--- data.js
+++ data.js
@@ -1,11 +1,12 @@
-const { json } = require('express');
 const express = require('express');
+const {body, validationResult} = require('express-validator');
 const fs = require('fs');
 const lodash = require('lodash');
+const Cryptr = require('cryptr');
+const cryptr = new Cryptr('token');
 
 const app = express();
-const port = 3000
-var loggedUserId;
+const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
@@ -22,8 +23,18 @@ app.use((req, res, next) => {
     next();
   });
 
-app.get('/', (req, res) => {
+app.get('/',(req,res) =>{
+  res.json(JSON.parse(fs.readFileSync('users.txt', 'utf8'))); 
+})
+
+app.get('/:id', (req, res) => {
     var listOftasks = [];
+    try {
+      var loggedUserId =JSON.parse(cryptr.decrypt(req.params.id)).id;
+    } catch(err) {
+      res.status(401).json();
+    }
+    
     try {
         const data =JSON.parse(fs.readFileSync('todolist.txt', 'utf8'));
         for (let task of data) {
@@ -38,12 +49,13 @@ app.get('/', (req, res) => {
     
 });
 
-app.post('/',(req, res, next) => {
+app.post('/',(req, res) => {
     var data=req.body;
+    data.userId = JSON.parse(cryptr.decrypt(data.userId)).id;
     var tasksList = JSON.parse(fs.readFileSync('todolist.txt', 'utf8'));
     if (lodash.find(tasksList,data) !== undefined) {
       tasksList.splice(lodash.findIndex(tasksList,data),1);
-    } else if (lodash.find(tasksList, obj => obj.id == data.id && obj.userId == loggedUserId) !== undefined){
+    } else if (lodash.find(tasksList, obj => obj.id == data.id && obj.userId == data.userId) !== undefined){
       tasksList.splice(lodash.findIndex(tasksList,obj => obj.id == data.id), 1, data);
     } else {
       tasksList.push(data);
@@ -57,22 +69,32 @@ app.post('/',(req, res, next) => {
       res.json();
 });
 
-app.get('/loggedUser',(req, res) => {
-  res.json(loggedUserId.toString());
-});
-
-app.post('/loggedUser',(req,res,next) =>  {
+app.post(
+  '/loggedUser',
+  body().custom(value =>{
+    var dataList =JSON.parse(fs.readFileSync('users.txt', 'utf8'));
+    if ( lodash.findIndex(dataList, (exp) => exp.username == value.username) === -1) {
+      throw new Error('Username not found');
+    }
+    return true;
+}).bail().custom(value =>{
+    var dataList =JSON.parse(fs.readFileSync('users.txt', 'utf8'));
+    if ( lodash.findIndex(dataList, (exp) => exp.password == value.password && exp.username == value.username) === -1) {
+      throw new Error('Password incorrect');
+    }
+    return true;
+}),
+(req,res) =>  {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    console.log('click')
+    return res.json({errors: errors.array()});
+  }
   var loggedUser = req.body;
   var userList = JSON.parse(fs.readFileSync('users.txt', 'utf8'));
   var index = lodash.find(userList , (exp) => exp.username === loggedUser.username);
-  if (index === undefined) {
-    res.json('noUser');
-  } else if(index.password !== loggedUser.password) {
-    res.json('noPass');
-  }else {
-    loggedUserId = index.id;
-    res.json('acces');
-  }
+  const encryptedID = cryptr.encrypt(JSON.stringify(index));
+  res.json(encryptedID);
 });
 
 app.get('/users', (req, res) => {
@@ -87,23 +109,37 @@ app.get('/users', (req, res) => {
   
 });
 
-app.post('/users',(req, res, next) => {
+app.post(
+  '/users',
+  body('username').custom(value =>{
+    var dataList =JSON.parse(fs.readFileSync('users.txt', 'utf8'));
+    if ( lodash.findIndex(dataList, (exp) => exp.username === value) !== -1) {
+      throw new Error('Username already in use');
+    }
+    return true;
+}),
+  body('password').custom((value, req) =>{
+    if ( value.length < 5) {
+      throw new Error('Password is too short');
+    }
+    return true;
+}),
+(req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    return res.json({errors: errors.array()});
+  }
   var data=req.body;
   var dataList =JSON.parse(fs.readFileSync('users.txt', 'utf8'));
-  if (lodash.findIndex(dataList, (exp) => exp.username === data.username) !== -1) {
-    res.json('exist');
-  }
-  else {
     data.id = dataList.length;
     dataList.push(data);
     fs.writeFile('users.txt', JSON.stringify(dataList), err => {
         if (err) {
           console.error(err)
-          return
+          return;
         }
       })
       res.json('');
-  }
 });
 
 app.listen(port, () => {
